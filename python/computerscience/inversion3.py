62f1b3035581fed54a41613c7d6db94e4023014f
Arno Rossouw <arno.rossouw@gmail.com>
2014-06-24 12:32:27 +0200

    Fixed inversion count on edge cases, split-inversions

--- python/computerscience/inversion3.py
+++ python/computerscience/inversion3.py
@@ -1,28 +1,85 @@
-def inversions(aList):
-	if not aList or type(aList) == type(1):
-		return 0
-	else:
-		inversion=0
-		end = len(aList)-1
-		y=1
-		x=0
+"""
+# do I correctly initialize and handle empty input?
+Input: {}
+expected Output: 0
 
-	if aList[0] > aList[end]:
-	     inversion+=1
+# do I correctly handle odd-sized input?
+I: {0}
+O: 0
 
-	if len(aList) == 1:
-	     return aList
-	else:
-	     while y <= end :	
-		if y == end:
-	 	   x += 1
-		   if (x + 2) <= end:
-		        y = x + 1		
-	 	   if x == end and aList[x] > aList[end]:
+I: {4294967295} -- try using your max int value
+O: 0
+
+# do I correclty handle even-size input?
+I: {0, 0}
+O: 0
+
+# do I detect basic case of split inversion?
+I: {1, 0}
+O: 1
+
+# do I actually count the split inversions?
+I: {1, 1, 0}
+O: 2
+
+# do I accumulate left-inversions?
+I: {1, 0, 1, 1}
+O: 1
+
+# do I accumulate right-inversions?
+I: {0, 0, 1, 0}
+O: 1
+
+# do I accumulate split-inversions together with sub-problem results?
+I: {1, 0, 1, 0}
+O: 3
+"""
+
+inversion = 0
+
+
+def merge(aList):
+    if not aList or type(aList) == type(1) or len(aList) == 1:
+	return 0
+    if len(aList) > 1:
+        mid = len(aList) / 2
+        left = aList[:mid]
+        right = aList[mid:]
+        merge(left)
+        merge(right)
+
+        left_length = len(left)
+        right_length = len(right)
+
+        i = 0
+        j = 1
+        k = 0
+
+        global inversion
+	split = 0
+
+        while i < left_length and k < right_length:
+	    if left[i] == right[k]:
+		split = 1   # is an equal list element count split-inversion
+		break
+	    if left[i] > right[k]:
+                inversion += 1
+                k += 1
+                j += 1
+            else:
+                i += 1
+                j += 1
+
+        k = 1
+        i = 0
+	if left_length == right_length and split:
+	     while i < left_length and k < right_length:
+		   if left[i] > right[k]:
 			inversion+=1
-	  	if aList[x] > aList[y]:
-			inversion+=1		
-		y+=1
-	return inversion
-	
-print inversions([1,0,1,0])
+		   k+=1
+		   j+=1
+	    
+
+        return inversion
+
+print merge([1,0,0,0])

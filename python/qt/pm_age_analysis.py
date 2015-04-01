3c386a317d2932b94b261899fd47fb1af8cd5de6
Arno Rossouw <arno.rossouw@workforce.co.za>
2015-04-01 12:16:13 +0200

    modified widget item generation, now generic

--- python/qt/pm_age_analysis.py
+++ python/qt/pm_age_analysis.py
@@ -37,59 +37,47 @@ class AppForm(QMainWindow):
         self.button.clicked.connect(self.clicked)
 
     def clicked(self):
-	setGroup(self.databases[self.database.currentIndex()])
-	conf = getProperties()
-	db = QSqlDatabase.addDatabase("QMYSQL")
-	db.setUserName(conf['username'])
-	db.setPassword(conf['password'])
-	db.setHostName(conf['host'])
-	db.setDatabaseName(conf['database'])
+        setGroup(self.databases[self.database.currentIndex()])
+        conf = getProperties()
+        db = QSqlDatabase.addDatabase("QMYSQL")
+        db.setUserName(conf['username'])
+        db.setPassword(conf['password'])
+        db.setHostName(conf['host'])
+        db.setDatabaseName(conf['database'])
         if (db.open() == False):
-	    QMessageBox.critical(self,db.lastError().text(),db.lastError().text())
+            QMessageBox.critical(self,db.lastError().text(),db.lastError().text())
 
-	result = QSqlQuery(sql_from_file('report_age_analysis.sql'))
-	result_text_err = str(result.lastError().text())
-	if len(result_text_err) > 1:
-             QMessageBox.critical(self,result.lastError().text(),result.lastError().text())
-	     db.close()
-	else:
-	     columns = []
-	     for i in range(0,result.record().count()):
-		     columns.append(str(result.record().fieldName(i)))
-	     self.table.setColumnCount(result.record().count())
-             self.table.setRowCount(result.size())
-             """ Column headers for UI table """
-             self.table.setHorizontalHeaderLabels(columns)
+        result = QSqlQuery(sql_from_file('report_age_analysis.sql'))
+        result_text_err = str(result.lastError().text())
+        
+        if len(result_text_err) > 1:
+            QMessageBox.critical(self,result.lastError().text(),result.lastError().text())
+            db.close()
+        else:
+            columns = []
+            for i in range(0,result.record().count()):
+                columns.append(str(result.record().fieldName(i)))
 
-
-             self.table.setMinimumSize(800,600)
-             self.table.setSelectionBehavior(self.table.SelectRows)
-             self.table.setSelectionMode(self.table.NoSelection)
-             self.table.setFocusPolicy(Qt.NoFocus)
-             self.table.setAlternatingRowColors(True)
-             self.table.verticalHeader().hide()
-             self.table.setShowGrid(False)
-             self.table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-
-             index=0
-             while (result.next()):
-                 self.table.setItem(index,0,QTableWidgetItem(result.value(0).toString()))
-                 self.table.setItem(index,1,QTableWidgetItem(result.value(1).toString()))
-                 self.table.setItem(index,2,QTableWidgetItem(result.value(2).toString()))
-                 self.table.setItem(index,3,QTableWidgetItem(result.value(3).toString()))
-                 self.table.setItem(index,4,QTableWidgetItem(result.value(4).toString()))
-                 self.table.setItem(index,5,QTableWidgetItem(result.value(5).toString()))
-                 self.table.setItem(index,6,QTableWidgetItem(result.value(6).toString()))
-                 index = index+1
-
-             self.table.show()
-
- 
-		
-
-	db.close()
-        #db.removeDatabase("QMYSQL")
+            self.table.setColumnCount(result.record().count())
+            self.table.setRowCount(result.size())
+            """ Column headers for UI table """
+            self.table.setHorizontalHeaderLabels(columns)
+            self.table.setMinimumSize(800,600)
+            self.table.setSelectionBehavior(self.table.SelectRows)
+            self.table.setSelectionMode(self.table.NoSelection)
+            self.table.setFocusPolicy(Qt.NoFocus)
+            self.table.setAlternatingRowColors(True)
+            self.table.verticalHeader().hide()
+            self.table.setShowGrid(False)
+            self.table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+            index=0
+            while (result.next()):
+                for i in range(0,len(columns)):
+                    self.table.setItem(index,i,QTableWidgetItem(result.value(i).toString()))
+                index = index+1
+            self.table.show()
+            db.close()
+        
 
 
 

8fa7a195aa659c1586594501ba688f43c32d3091
Arno Rossouw <arno.rossouw@workforce.co.za>
2015-06-03 16:02:58 +0200

    Example of calculating an employee's pay based on hourly pay rate, including overtime and doubletime

---
+++ databases/postgres/pgsql-payroll-calculate_pay.sql
@@ -0,0 +1,32 @@
+SELECT clockno,SUM(CASE WHEN ct.type = 'Normal Time' 
+	       THEN er.rate*date_part('hour', checkout - checkin)
+	       WHEN ct.type = 'Over Time'
+	       THEN er.rate*date_part('hour', checkout - checkin) * 1.5
+	       WHEN ct.type = 'Double Time'
+	       THEN er.rate*date_part('hour', checkout - checkin) * 2
+	       END)
+from timesheet t 
+inner join client_work_hours cw on 
+	t.client_id = cw.client_id 
+inner join work_hours_type ct on 
+	ct.id = cw.hour_type
+inner join employee_rate er on
+       er.emp_no =  t.clockno
+group by t.clockno
+
+SELECT clockno,CASE WHEN ct.type = 'Normal Time' 
+	       THEN er.rate*date_part('hour', checkout - checkin)
+	       WHEN ct.type = 'Over Time'
+	       THEN er.rate*date_part('hour', checkout - checkin) * 1.5
+	       WHEN ct.type = 'Double Time'
+	       THEN er.rate*date_part('hour', checkout - checkin) * 2
+	       END
+from timesheet t 
+inner join client_work_hours cw on 
+	t.client_id = cw.client_id 
+inner join work_hours_type ct on 
+	ct.id = cw.hour_type
+inner join employee_rate er on
+       er.emp_no =  t.clockno
+group by t.clockno
+

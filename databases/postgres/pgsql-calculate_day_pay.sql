67f7acc8526214c86917fb097aed489be38df666
Arno Rossouw <arnor@workforce.co.za>
2015-09-02 16:07:19 +0200

    fixed innacuracy with payrate calculation function refactored design

--- databases/postgres/pgsql-calculate_day_pay.sql
+++ databases/postgres/pgsql-calculate_day_pay.sql
@@ -1,6 +1,6 @@
-create function sp_calc_pay(timeid_l integer, emp_no_l integer) RETURNS numeric AS $$
+create function sp_calc_pay(timeid_l integer,clockno_l integer) RETURNS numeric AS $$
 DECLARE
-	l_employee_rate numeric := 0.0;
+	l_employee_hourly numeric := 0.0;
 	l_employee_category varchar := '';
 	employee_pay numeric := 0.0;
 	l_day timestamp := now();
@@ -8,10 +8,10 @@ DECLARE
 BEGIN
 	SELECT checkin into l_day from timesheet where timeid = timeid_l;
 	SELECT to_char(checkin::date, 'Day') INTO l_day_of_week from timesheet where timeid = timeid_l;
-	SELECT rate INTO l_employee_rate FROM employee_rate where emp_no = emp_no_l;
+	SELECT rate INTO l_employee_hourly FROM employee_hourly where clockno =clockno_l;
 	SELECT trim(to_char(l_day::date, 'Day')) INTO l_day_of_week;
 
-	SELECT l_employee_rate * EXTRACT(EPOCH FROM checkout - checkin) / 60 * 0.016667
+	SELECT l_employee_hourly * EXTRACT(EPOCH FROM checkout - checkin) / 3600
 		INTO employee_pay
 		FROM timesheet WHERE timeid = timeid_l;
 
--- databases/postgres/pgsql-payroll.sql
+++ databases/postgres/pgsql-payroll.sql
@@ -8,114 +8,142 @@ create table employee (
    active boolean NOT NULL DEFAULT TRUE
 );
 
-create table job_history (
+create table employee_category (
+    clockno int not null references employee(clockno),
+	category_name varchar(30) NOT NULL,
+	UNIQUE(category_name)
+);
+
+create table employment_history (
     clockno int not null references employee(clockno),
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	title varchar(35),
+	title varchar(35) NOT NULL,
 	CHECK (from_date < to_date)
 );
 
-create table hr_leave_type (
+create table employee_leave_type (
      id serial primary key,
-     leave_name varchar(15)
+     leave_type varchar(15) NOT NULL,
+	 UNIQUE(leave_type)
 );
 
-create table hr_leave (
+create table employee_leave (
 	approved_by int not null references employee(clockno),
-    from_date DATE,
-    to_date DATE,
-	leave_type_id int references hr_leave_type(id),
-	taken int NOT NULL,
-	days_left int NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	leave_type_id int references employee_leave_type(id) NOT NULL,
 	CHECK (from_date < to_date)
 );
 
-create table hr_deduction (
-	deductid serial primary key,
-	clockno int not null references employee(clockno),
-	deductiontype varchar(20),
-	amount real,
-	note TEXT
+create table employee_deduction_type (
+	id serial primary key,
+    description varchar(35) NOT NULL,
+	UNIQUE(description)
 );
+	
 
+create table employee_deduction (
+	clockno int not null references employee(clockno),
+	deductiontype int not null references employee_deduction_type(id),
+	amount real NOT NULL,
+	deductiondate DATE NOT NULL
+);
 
 create table department (
     dept_no serial primary key,
-    dept_name varchar(25)
+    dept_name varchar(25) NOT NULL
 );
 
 create table employee_dept (
-     clockno int not null references employee(clockno), 
-     dept_no int references department(dept_no)
-);
-
-create table job_salary (
-	title varchar(20) not null,
-	minimum money,
-	maximum money,
-    CHECK (maximum > minimum),
-	UNIQUE(title)	
+     clockno int not null references employee(clockno) NOT NULL, 
+     dept_no int references department(dept_no) NOT NULL
 );
 
 
 create table bank_account_type (
    id serial primary key,
-   type varchar(20),
+   type varchar(20) NOT NULL,
    UNIQUE(type)
+);
+
+create table bank_name (
+   id serial primary key,
+   bank_name varchar(20) NOT NULL,
+   UNIQUE(bank_name)
+);
+
+create table employee_bank_account (
+	emp_no int not null references employee(clockno) NOT NULL,
+	bank_account_type_id int references bank_account_type(id) NOT NULL,
+	account_no varchar(20) NOT NULL,
+	account_branch varchar(20) NOT NULL,
+	account_bankname int references bank_name(id) NOT NULL
 );
 
 create table client_account (
    client_id int primary key,
-   bank_account_type_id int references bank_account_type(id),
-   account_no varchar(20),
-   account_branch varchar(30),
-   account_bank varchar(20)
+   bank_account_type_id int references bank_account_type(id) NOT NULL,
+   account_no varchar(20) NOT NULL,
+   account_branch varchar(30) NOT NULL,
+   account_bankname int references bank_name(id) NOT NULL
 );
 
 create table client (
      client_id serial primary key,
-     client_name varchar(30)
+     client_name varchar(30) NOT NULL,
+	UNIQUE(client_name)
 );
 
 
 create table client_branch (
-   client_id int references client(client_id),
+   client_id int references client(client_id) NOT NULL,
    branch_id serial primary key,
-   address TEXT,
-   branch_name varchar(40),
+   address TEXT NOT NULL,
+   branch_name varchar(40) NOT NULL,
    phone varchar(15),
    email varchar(35),
    UNIQUE(branch_name)
 );
 
+create table project (
+	id serial primary key,
+	client_id int not null references client(client_id),
+	title varchar(30) NOT NULL,
+	description varchar(45) NOT NULL,
+	startdate timestamp NOT NULL,
+	enddate	  timestamp NULL,
+	createdby int not null references employee(clockno),
+	lastupdatedby int not null references employee(clockno),
+	UNIQUE(title)
+);
+
 create table timesheet (
 	timeid serial primary key,
-	clockno int not null references employee(clockno),
-	checkin timestamp,
-	checkout timestamp,
-	hours	real,
-	description varchar(30),
-	client_id int references client(client_id),
+	clockno int not null references employee(clockno) NOT NULL,
+	checkin timestamp NOT NULL,
+	checkout timestamp NOT NULL,
+	description varchar(30) NOT NULL,
+	project_id int references project(id) NOT NULL,
 	CHECK (checkout > checkin)
 );
 	
 
 create table company_bank_account (
-   bank_account_type_id int references bank_account_type(id),
-   account_no   varchar(30),
-   account_branch varchar(30),
-   account_bank varchar(30),
+   bank_account_type_id int references bank_account_type(id) NOT NULL,
+   account_no   varchar(30) NOT NULL,
+   account_branch varchar(30) NOT NULL,
+   account_bank varchar(30) NOT NULL,
   UNIQUE(account_no)
 );
 
 create table payroll (
-    payroll_id serial primary key,
+    payroll_id int not null,
     employee_id int not null references employee(clockno),
-    hoursworked  int,
-    grosspay     real,
-    deductions   real,
-    netpay	real
+    hoursworked  int not null,
+    grosspay     real not null,
+    deductions   real not null,
+    netpay	real not null
 );
 
 create table account_transaction_type (
